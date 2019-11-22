9577a01a570045979fd1e8a9b31640fb1ef0c785
Paulina Gavilan <pauvpg@gmail.com>
2019-11-22 14:49:15 +0100

    package.json, package-lock.json & gatsby-config.js changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,13 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
